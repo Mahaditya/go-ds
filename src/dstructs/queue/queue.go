e40238e98ce31fb39a126eae5203373ce2249d6c
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-12-03 14:42:19 +0530

    Implements queue

---
+++ src/dstructs/queue/queue.go
@@ -0,0 +1,34 @@
+package queue
+
+import "errors"
+
+type Queue[T any] struct{
+	data []T
+}
+
+func (q *Queue[T]) Size() int{
+	return len(q.data)
+}
+
+func (q *Queue[T]) Push(data T){
+	q.data = append(q.data,data)
+}
+
+func (q *Queue[T]) Front() (T,error){
+	if(q.Size()>0){
+		return q.data[0],nil
+	}
+	return *new(T),errors.New("queue is empty") 
+}
+
+func (q *Queue[T]) Pop() (error){
+	if(q.Size()>0){
+		q.data=q.data[1:]
+		return nil
+	}
+	return errors.New("popping an empty queue")
+}
+
+func (q *Queue[T]) NotEmpty() bool {
+	return q.Size()>0
+}
