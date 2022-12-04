8ff70dc2994b89861aee18d0de3090f50968f952
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-12-04 08:22:44 +0530

    Minor Refactor

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main(){
 	for i:=0;i<1000;i++{
 		bst.Insert(rand.Intn(1000),rand.Intn(1000))
 	}
+	fmt.Println(bst.Search(456)) 
 	fmt.Println(bst.Search(2))
 	bst.Print()
 }
--- src/dstructs/bst/bst.go
+++ src/dstructs/bst/bst.go
@@ -6,41 +6,19 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-type binarySearchTreeNode[K constraints.Ordered, T any] struct {
-	left  *binarySearchTreeNode[K, T]
-	right *binarySearchTreeNode[K, T]
+type bstNode[K constraints.Ordered, T any] struct {
+	left  *bstNode[K, T]
+	right *bstNode[K, T]
 	key   K
 	value T
 }
 
 type BinarySearchTree[K constraints.Ordered, T any] struct {
-	root *binarySearchTreeNode[K, T]
+	root *bstNode[K, T]
 	size uint64
 }
 
-func (bst *BinarySearchTree[K, T]) Create(key K, val T) *BinarySearchTree[K, T] {
-	return bst.Insert(key,val)
-}
-
-func (bst *BinarySearchTree[K, T]) Insert(key K, val T) *BinarySearchTree[K, T] {
-	if bst.root == nil {
-		bst.root = &binarySearchTreeNode[K, T]{nil, nil, key, val}
-		bst.size++
-		return bst
-	}
-	_, lowerLimit := bst.search(key)
-
-	if lowerLimit.key < key {
-		lowerLimit.right = &binarySearchTreeNode[K, T]{nil, nil, key, val}
-		bst.size++
-	} else {
-		lowerLimit.left = &binarySearchTreeNode[K, T]{nil, nil, key, val}
-		bst.size++
-	}
-	return bst
-}
-
-func (bst *BinarySearchTree[K, T]) search(searchKey K) (found bool, value *binarySearchTreeNode[K, T]) {
+func (bst *BinarySearchTree[K, T]) search(searchKey K) (found bool, value *bstNode[K, T]) {
 	if bst.root == nil {
 		return false, bst.root
 	}
@@ -57,6 +35,70 @@ func (bst *BinarySearchTree[K, T]) search(searchKey K) (found bool, value *binar
 	return false, node
 }
 
+func (bst *BinarySearchTree[K, T]) levelOrderTraversal() [][]*bstNode[K,T] {
+	levelOrder:= make([][]*bstNode[K,T],0)
+	q:= new(queue.Queue[*bstNode[K,T]])
+	q.Push(bst.root)
+	for q.NotEmpty(){
+		thisLevel:= make([]*bstNode[K,T],0)
+		for n:= q.Size();n>0;n-- {
+			node,_ := q.Front()
+			q.Pop()
+			thisLevel=append(thisLevel, node)
+			if node.left!=nil {
+				q.Push(node.left)
+			}
+			if node.right!=nil{
+				q.Push(node.right)
+			}
+		}
+		levelOrder=append(levelOrder, thisLevel)
+	}
+	return levelOrder
+}
+
+func inOrderHelper[K constraints.Ordered, T any](root *bstNode[K, T], inOrderContainer *([]*bstNode[K, T])) {
+	if root == nil {
+		return
+	}
+	inOrderHelper(root.left, inOrderContainer)
+	*inOrderContainer = append((*inOrderContainer), root)
+	inOrderHelper(root.right, inOrderContainer)
+}
+
+func (bst *BinarySearchTree[K, T]) inOrder() []*bstNode[K, T] {
+	inOrderContainer := make([]*bstNode[K, T], 0)
+	inOrderHelper(bst.root, &inOrderContainer)
+	return inOrderContainer
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+func (bst *BinarySearchTree[K, T]) Create(key K, val T) *BinarySearchTree[K, T] {
+	return bst.Insert(key,val)
+}
+
+func (bst *BinarySearchTree[K, T]) Insert(key K, val T) *BinarySearchTree[K, T] {
+	if bst.root == nil {
+		bst.root = &bstNode[K, T]{nil, nil, key, val}
+		bst.size++
+		return bst
+	}
+	_, lowerLimit := bst.search(key)
+
+	if lowerLimit.key < key {
+		lowerLimit.right = &bstNode[K, T]{nil, nil, key, val}
+		bst.size++
+	} else {
+		lowerLimit.left = &bstNode[K, T]{nil, nil, key, val}
+		bst.size++
+	}
+	return bst
+}
+
+
+
 func (bst *BinarySearchTree[K, T]) Search(searchKey K) (found bool, val T) {
 	found, node := bst.search(searchKey)
 	if node != nil {
@@ -67,21 +109,6 @@ func (bst *BinarySearchTree[K, T]) Search(searchKey K) (found bool, val T) {
 
 func (bst *BinarySearchTree[K, T]) Size() uint64 {
 	return bst.size
-}
-
-func inOrderHelper[K constraints.Ordered, T any](root *binarySearchTreeNode[K, T], inOrderContainer *([]*binarySearchTreeNode[K, T])) {
-	if root == nil {
-		return
-	}
-	inOrderHelper(root.left, inOrderContainer)
-	*inOrderContainer = append((*inOrderContainer), root)
-	inOrderHelper(root.right, inOrderContainer)
-}
-
-func (bst *BinarySearchTree[K, T]) inOrder() []*binarySearchTreeNode[K, T] {
-	inOrderContainer := make([]*binarySearchTreeNode[K, T], 0)
-	inOrderHelper(bst.root, &inOrderContainer)
-	return inOrderContainer
 }
 
 func (bst *BinarySearchTree[K, T]) InOrderKeys() []K {
@@ -100,28 +127,6 @@ func (bst *BinarySearchTree[K, T]) InOrderValues() []T {
 		inOrderValuesContainer = append(inOrderValuesContainer, node.value)
 	}
 	return inOrderValuesContainer
-}
-
-func (bst *BinarySearchTree[K, T]) levelOrderTraversal() [][]*binarySearchTreeNode[K,T] {
-	levelOrder:= make([][]*binarySearchTreeNode[K,T],0)
-	q:= new(queue.Queue[*binarySearchTreeNode[K,T]])
-	q.Push(bst.root)
-	for q.NotEmpty(){
-		thisLevel:= make([]*binarySearchTreeNode[K,T],0)
-		for n:= q.Size();n>0;n-- {
-			node,_ := q.Front()
-			q.Pop()
-			thisLevel=append(thisLevel, node)
-			if node.left!=nil {
-				q.Push(node.left)
-			}
-			if node.right!=nil{
-				q.Push(node.right)
-			}
-		}
-		levelOrder=append(levelOrder, thisLevel)
-	}
-	return levelOrder
 }
 
 
