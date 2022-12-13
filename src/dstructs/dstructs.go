e1cdf8fe8e765bf976a77a359275992d0a6de370
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-12-13 14:43:37 +0530

    Adds more interfaces

--- src/dstructs/dstructs.go
+++ src/dstructs/dstructs.go
@@ -2,6 +2,8 @@ package dstructs
 
 type Sizeable interface {
 	Size() int
+	IsEmpty() bool
+	IsNotEmpty() bool
 }
 
 type Indexable[T any] interface {
@@ -15,3 +17,44 @@ type Popable[T any] interface {
 type Pushable[T any] interface {
 	Push(el T)
 }
+
+type TopPeekable[T any] interface {
+	Top() (T,error)
+}
+
+type FrontPeekable[T any] interface {
+	Front() (T,error)
+}
+
+type EndAccessible[T any] interface {
+	First() (T,error)
+	Last() (T,error)
+}
+
+type Searchable[T any] interface {
+	Contains(T) bool
+}
+
+type Stack[T any] interface {
+	Popable[T]
+	Pushable[T]
+	Sizeable
+	TopPeekable[T]
+}
+
+type Queue[T any] interface {
+	Popable[T]
+	Pushable[T]
+	FrontPeekable[T]
+	Sizeable
+}
+
+type Vector[T any] interface {
+	Popable[T]
+	Pushable[T]
+	Indexable[T]
+	EndAccessible[T]
+	Searchable[T]
+	Sizeable
+}
+
