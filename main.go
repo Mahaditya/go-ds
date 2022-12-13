98de7073d869cecc70acf4f6d4ca4b605b1a07a3
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-12-13 14:45:58 +0530

    Empty the main package

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"go-ds/src/dstructs"
-	"go-ds/src/dstructs/queue"
-)
-
-func call(st dstructs.Queue[string]){
-	st.Push("lambda")
-}
-
-func main() {
-	var myQueue queue.Queue[string]
-	myQueue.Push("alpha")
-	myQueue.Push("beta")
-	myQueue.Push("gamma")
-	fmt.Println(myQueue)
-	
-}
+func main() {}
