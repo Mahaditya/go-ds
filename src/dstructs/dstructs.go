4c6c33c515dafefefdc56d1e9ac9f9c81ce977c2
Mahaditya Kaushik <33066858+Mahaditya@users.noreply.github.com>
2022-12-13 11:24:26 +0530

    Adds Interfaces

--- src/dstructs/dstructs.go
+++ src/dstructs/dstructs.go
@@ -1,1 +1,17 @@
 package dstructs
+
+type Sizeable interface {
+	Size() int
+}
+
+type Indexable[T any] interface {
+	At(int)(T,error)
+}
+
+type Popable[T any] interface {
+	Pop() (T,error)
+}
+
+type Pushable[T any] interface {
+	Push(el T)
+}
